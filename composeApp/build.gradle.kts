31ce4da7442809f547ea3ede970746b43bb03962
ivan <ivan@ivk1800.ru>
2025-08-29 21:42:56 +0400

    deploy

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -71,3 +71,63 @@ compose.desktop {
 tasks.withType<ComposeHotRun>().configureEach {
     mainClass = "MainKt"
 }
+
+tasks.register("buildApp") {
+    dependsOn("jsBrowserDistribution")
+
+    val distDir = layout.buildDirectory.dir("dist/js/productionExecutable")
+    val buildTimestamp: String by extra { System.currentTimeMillis().toString() }
+
+    doLast {
+        val dist = distDir.get().asFile
+        if (!dist.exists()) {
+            throw GradleException("Distribution directory not found: $distDir")
+        }
+
+        val composeResourcesDir = File(dist, "composeResources")
+        if (!composeResourcesDir.exists()) {
+            throw GradleException("composeResources not found in $dist")
+        }
+        val newComposeResourcesDirName = "composeResources-${buildTimestamp}"
+        composeResourcesDir.renameTo(File(dist, newComposeResourcesDirName))
+
+        val targetDir = File(dist, buildTimestamp)
+        targetDir.mkdirs()
+
+        dist.listFiles()
+            ?.filter { it.isFile && (it.name.startsWith("composeApp") || it.name.startsWith("skiko")) }
+            ?.forEach { file ->
+                file.copyTo(File(targetDir, file.name), overwrite = true)
+                file.delete()
+            }
+
+        val index = File(dist, "index.html")
+        if (!index.exists()) {
+            throw GradleException("index.html not found in $dist")
+        }
+
+        val updatedIndex = index.readText()
+            .replace(
+                """<script\s+src=["']composeApp\.js["']></script>""".toRegex(),
+                """<script src="$buildTimestamp/composeApp.js"></script>""",
+            )
+            .replace(
+                """<script\s+src=["']skiko\.js["']></script>""".toRegex(),
+                """<script src="$buildTimestamp/skiko.js"></script>""",
+            )
+
+        index.writeText(updatedIndex)
+
+        val composeAppFile = File(targetDir, "composeApp.js")
+        if (!composeAppFile.exists()) {
+            throw GradleException("composeApp.js not found in $dist")
+        }
+
+        val newComposeAppContent = composeAppFile.readText()
+            .replace(
+                "composeResources/recomposition_visualization.composeapp.generated.resources/",
+                "$newComposeResourcesDirName/recomposition_visualization.composeapp.generated.resources/",
+            )
+        composeAppFile.writeText(newComposeAppContent)
+    }
+}
