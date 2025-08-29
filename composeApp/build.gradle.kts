e027390e0fd66cd4c33716e677148db7506da214
ivan <ivan@ivk1800.ru>
2025-08-29 23:06:55 +0400

    metrika

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -110,6 +110,22 @@ tasks.register("buildApp") {
             throw GradleException("index.html not found in $dist")
         }
 
+        val metrikaCounter = """
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103948613', 'ym');
+
+    ym(103948613, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/103948613" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+    """.trimIndent()
+
         val updatedIndex = index.readText()
             .replace(
                 """<script\s+src=["']composeApp\.js["']></script>""".toRegex(),
@@ -119,30 +135,7 @@ tasks.register("buildApp") {
                 """<script\s+src=["']skiko\.js["']></script>""".toRegex(),
                 """<script src="$buildTimestamp/skiko.js"></script>""",
             )
-            .run {
-                val metrikaId = metrikaIdProvider.get()
-                if (metrikaId.isNotBlank()) {
-                    val metrikaCounter = """
-            <!-- Yandex.Metrika counter -->
-            <script type="text/javascript">
-                (function(m,e,t,r,i,k,a){
-                    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                    m[i].l=1*new Date();
-                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${metrikaId}', 'ym');
-
-                ym(${metrikaId}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-            </script>
-            <noscript><div><img src="https://mc.yandex.ru/watch/${metrikaId}" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-            <!-- /Yandex.Metrika counter -->
-    """.trimIndent()
-
-                    replace("<!--METRIKA_PLACEHODLER-->", metrikaCounter)
-                } else {
-                    this
-                }
-            }
+            .replace("<!--METRIKA_PLACEHODLER-->", metrikaCounter)
 
         index.writeText(updatedIndex)
 
