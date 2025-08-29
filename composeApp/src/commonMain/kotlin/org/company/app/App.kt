d302a19ba11c0684e5eb07248c295f4a46b33d53
ivan <ivan@ivk1800.ru>
2025-08-29 21:55:37 +0400

    1

--- composeApp/src/commonMain/kotlin/org/company/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/App.kt
@@ -1,24 +1,42 @@
 package org.company.app
 
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import multiplatform_app.composeapp.generated.resources.*
+import kotlinx.coroutines.isActive
+import multiplatform_app.composeapp.generated.resources.Res
+import multiplatform_app.composeapp.generated.resources.cyclone
+import multiplatform_app.composeapp.generated.resources.open_github
+import multiplatform_app.composeapp.generated.resources.run
+import multiplatform_app.composeapp.generated.resources.stop
+import multiplatform_app.composeapp.generated.resources.theme
 import org.company.app.theme.AppTheme
 import org.company.app.theme.LocalThemeIsDark
-import kotlinx.coroutines.isActive
-import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.resources.stringResource
-import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Preview
@@ -33,7 +51,6 @@ internal fun App() = AppTheme {
     ) {
         Text(
             text = stringResource(Res.string.cyclone),
-            fontFamily = FontFamily(Font(Res.font.IndieFlower_Regular)),
             style = MaterialTheme.typography.displayLarge
         )
 
@@ -51,23 +68,12 @@ internal fun App() = AppTheme {
             }
         }
 
-        Image(
-            modifier = Modifier
-                .size(250.dp)
-                .padding(16.dp)
-                .run { rotate(rotate.value) },
-            imageVector = vectorResource(Res.drawable.ic_cyclone),
-            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
-            contentDescription = null
-        )
-
         ElevatedButton(
             modifier = Modifier
                 .padding(horizontal = 8.dp, vertical = 4.dp)
                 .widthIn(min = 200.dp),
             onClick = { isRotating = !isRotating },
             content = {
-                Icon(vectorResource(Res.drawable.ic_rotate_right), contentDescription = null)
                 Spacer(Modifier.size(ButtonDefaults.IconSpacing))
                 Text(
                     stringResource(if (isRotating) Res.string.stop else Res.string.run)
@@ -76,16 +82,11 @@ internal fun App() = AppTheme {
         )
 
         var isDark by LocalThemeIsDark.current
-        val icon = remember(isDark) {
-            if (isDark) Res.drawable.ic_light_mode
-            else Res.drawable.ic_dark_mode
-        }
 
         ElevatedButton(
             modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp).widthIn(min = 200.dp),
             onClick = { isDark = !isDark },
             content = {
-                Icon(vectorResource(icon), contentDescription = null)
                 Spacer(Modifier.size(ButtonDefaults.IconSpacing))
                 Text(stringResource(Res.string.theme))
             }
